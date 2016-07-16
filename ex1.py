9af1ab4c8d6abe4e228d4ff851464db7b2add31c
Paul Kramme <pjkramme@gmail.com>
2016-07-16 17:00:02 -0400

    Add exercise 1

---
+++ ex1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+print("Hello World!")
+print("Hello again.")
+print("I like typing this")
+print("This is fun")
+print("Yay printing")
+print("I'd rather you'd 'not'.")
+print('I sad do "not" touch this')
